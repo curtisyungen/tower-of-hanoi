ea8d9e1933ef03079932376ec6396cf6ce7ca2f1
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-13 18:41:43 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 require("dotenv").config();
 
 const express = require("express");
-
-var app = express();
+const app = express();
 const http = require("http").Server(app);
-
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("client/build"));
-app.use(routes);
 
-var syncOptions = { force: false };
-
-if (process.env.NODE_ENV === "test") {
-    syncOptions.force = true;
-}
-
-db.sequelize.sync(syncOptions).then(function() {
-    http.listen(PORT, function() {
-        console.log("Listening on port ", PORT, ". Visit http://localhost:5000 in your browser.");
-    });
+http.listen(PORT, function() {
+    console.log("Listening on port ", PORT, ". Visit http://localhost:5000 in your browser.");
 });
 
 module.exports.app = app;
