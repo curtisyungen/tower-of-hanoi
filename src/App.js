2554ef56ba44e375b0affa73e6c883b8a36be6b2
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-13 19:02:30 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React, { Component } from 'react';
+import { createStore } from "redux";
 import './App.css';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "SUBTRACT":
+      break;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+  console.log("Store updated", store.getState());
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 80
+});
 
 class App extends Component {
 
