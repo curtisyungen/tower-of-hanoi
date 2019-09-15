eae330464f04db3c00e177a841789af85c562df4
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-14 22:23:46 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import React, { Component } from 'react';
-import { createStore } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import './App.css';
 
-const reducer = (state, action) => {
+const mathReducer = (state = {
+  result: 1,
+  lastValues: [],
+}, action) => {
   switch (action.type) {
     case "ADD":
-      state = state + action.payload;
+      state = {
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, action.payload],
+      };
       break;
     case "SUBTRACT":
+        state = {
+          ...state,
+          result: state.result - action.payload,
+          lastValues: [...state.lastValues, action.payload],
+        };
       break;
   }
   return state;
 }
 
-const store = createStore(reducer, 1);
+const userReducer = (state = {
+  name: "Curtis",
+  age: 29,
+}, action) => {
+  switch (action.type) {
+    case "SET_NAME":
+      state = {
+        ...state,
+        name: action.payload,
+      };
+      break;
+    case "SET_AGE":
+        state = {
+          ...state,
+          age: action.payload,
+        };
+      break;
+  }
+  return state;
+}
 
-store.subscribe(() => {
-  console.log("Store updated", store.getState());
-});
+const myLogger = (store) => (next) => (action) => {
+  console.log("Logged action", action);
+  next(action);
+}
+
+const store = createStore(
+  combineReducers({mathReducer, userReducer}), 
+  {}, 
+  applyMiddleware(logger)
+);
+
+// store.subscribe(() => {
+//   console.log("Store updated", store.getState());
+// });
 
 store.dispatch({
   type: "ADD",
@@ -27,6 +70,11 @@ store.dispatch({
 store.dispatch({
   type: "ADD",
   payload: 80
+});
+
+store.dispatch({
+  type: "SET_AGE",
+  payload: 30
 });
 
 class App extends Component {
