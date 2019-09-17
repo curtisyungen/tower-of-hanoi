2fd96ee26044dcf7c2e20970e3994adf68b0f21e
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-17 13:50:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from "react-redux";
 import App from './App';
+import store from "./redux/store.js";
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// The Provider component  makes the redux store available
+// to all container components in the application
+// without having to pass it explicitly as a prop
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
