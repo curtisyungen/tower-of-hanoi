15e2252a32b91d922a953ff56dc07d732e64548f
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-18 06:48:55 -0700

    Add files via upload

--- src/components/ring.js
+++ src/components/ring.js
@@ -7,14 +7,14 @@ const Ring = ({ ringId, vertPosition, tower, clickRing }) => {
             className={`ring`}
             onMouseDown={(event) => clickRing(ringId, tower, event)}
             style={{
-                backgroundColor: `rgb(${ringId * 30},${ringId * 30}, ${ringId * 30})`,
+                backgroundColor: `rgb(${ringId * 30},100, ${ringId * 30})`,
                 bottom: `${vertPosition * 50}px`,
                 cursor: "pointer",
                 height: "50px",
                 left: `${tower * 25}%`,
                 position: "absolute",
-                transform: `translate(${-(ringId * 15) + 25}px)`,
-                width: `${(ringId * 50) + 20}px`,
+                transform: `translate(${-(ringId * 30) + 25}px)`,
+                width: `${ringId * 60}px`,
                 zIndex: 10,
             }}
         >
