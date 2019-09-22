79602f2edee804741cbf3b9c6c980f77de50a8a2
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-22 09:10:56 -0700

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,9 @@ import reducer from "./reducers";
 // An object that holds application state.
 // ====================================================================
 
+// window.STATE_FROM_SERVER: initial state. Useful for hydrating state of client to match state of a Redux app. running on server.
+// window.__REDUX_DEVTOOLS_EXTENSION__: activates Redux Development Tools in Chrome browser window
+
 const store = createStore(reducer, window.STATE_FROM_SERVER, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
